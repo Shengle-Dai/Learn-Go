695b2967782d79eb4c748b719f13bbc43322c7ee
Frank <sd924@cornell.edu>
2025-03-07 03:15:08 -0500

    finish hello world

--- hello.go
+++ hello.go
@@ -4,13 +4,22 @@ import "fmt"
 
 const englishHelloPrefix = "Hello, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+	prefix := englishHelloPrefix
+
+	switch language {
+	case "Spanish":
+		prefix = "Hola, "
+	case "French":
+		prefix = "Bonjour, "
+	}
+
+	return prefix + name
 }
 
 func main() {
-	fmt.Println(Hello("bro"))
+	fmt.Println(Hello("bro", ""))
 }
